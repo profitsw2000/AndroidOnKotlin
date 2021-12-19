b72830bd9afbe5da5c16e3080b14f87aac21b383
profitsw2000 <profitsw2000@gmail.com>
2021-12-19 19:18:27 +0300

    A button has been added to main activity and click listener has been set on it. When button is pressed textView with text becomes visible.

--- app/src/main/java/ru/profitsw2000/androidonkotlin/MainActivity.kt
+++ app/src/main/java/ru/profitsw2000/androidonkotlin/MainActivity.kt
@@ -2,11 +2,22 @@ package ru.profitsw2000.androidonkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import ru.profitsw2000.androidonkotlin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clickMessage: TextView = findViewById(R.id.click_text)  ;
+
+        val button: Button = findViewById(R.id.button_listener) ;
+        button.setOnClickListener{
+            clickMessage.visibility = View.VISIBLE  ;
+            clickMessage.text = "Button clicked!!!" ;
+        }
     }
 }
