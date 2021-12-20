48b93138f6d6d623944287b1eea0ee3ce6c23ed1
profitsw2000 <profitsw2000@gmail.com>
2021-12-20 22:13:01 +0300

    Created Object and call copy() function to display class fields on the screen.

--- app/src/main/java/ru/profitsw2000/androidonkotlin/MainActivity.kt
+++ app/src/main/java/ru/profitsw2000/androidonkotlin/MainActivity.kt
@@ -13,12 +13,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val clickMessage: TextView = findViewById(R.id.click_text)  ;
+        val output: TextView = findViewById(R.id.objectText)    ;
 
         val button: Button = findViewById(R.id.button_listener) ;
         button.setOnClickListener{
             clickMessage.visibility = View.VISIBLE  ;
             val animal = Animal("Cat", "Bob", 5)
             clickMessage.text = animal.kind + ", " + animal.name + ", " + animal.age;
+
+            //create object, copy() and display it
+            val dog = object {
+                val name = "Bark"
+                val age = 3
+                fun copy(){
+                    output.text = "Dog name is $name and it is $age years old"
+                }
+            }   ;
+            dog.copy()  ;
         }
     }
 }
