6a7ef13d5c3c55d5b3b58385f3dc8942967e2766
profitsw2000 <profitsw2000@gmail.com>
2021-12-21 21:51:39 +0300

    Had been make 2 cycles and display values in Logcat through it.

--- app/src/main/java/ru/profitsw2000/androidonkotlin/MainActivity.kt
+++ app/src/main/java/ru/profitsw2000/androidonkotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.profitsw2000.androidonkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -11,6 +12,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val animalList: List<Animal> = listOf(Animal("Dog", "Caddy", 9), Animal("Croc", "Jane", 39), Animal("Lion", "Maasai", 10))
+        for (animal in animalList) {
+            Log.d("AnmlDbg", animal.kind + ": " + animal.name + " " + animal.age)
+        }
+
+        for (i in 1..10) {
+            Log.d("Cycle", "Round " + i)    ;
+        }
 
         val clickMessage: TextView = findViewById(R.id.click_text)  ;
         val output: TextView = findViewById(R.id.objectText)    ;
