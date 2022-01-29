d88b2068e7318a8c6b4d7e5e956689dea33ab8d3
profitsw2000 <profitsw2000@gmail.com>
2022-01-29 11:55:26 +0300

    Изменены свойства TextView и создан класс для приёма широковещательных сообщений MessageReceiver.kt.

---
+++ app/src/main/java/ru/profitsw2000/MessageReceiver.kt
@@ -0,0 +1,13 @@
+package ru.profitsw2000
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class MessageReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("MessageReceiver.onReceive() is not implemented")
+    }
+}
